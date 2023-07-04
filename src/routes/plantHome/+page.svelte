df050399e4d213323a8cbd1cf6fa11ea93aad879
BuildWithCloud <skybox_spells.0g@icloud.com>
2023-07-04 19:03:06 +0100

    moved top bar to own file

--- src/routes/plantHome/+page.svelte
+++ src/routes/plantHome/+page.svelte
@@ -1,7 +1,9 @@
+
 <script>
     import Logo from '$lib/images/Logo.png';
     import Bell from '$lib/images/Bell.png';
     import Menu from './menu/+page.svelte';
+    import TopBar from './topBar/+page.svelte';
 </script>
 <style>
     .grid-container {
@@ -28,18 +30,15 @@
         align-items: center;
     }
     
+    
     .topBarGrid{
         display: grid;
         grid-template-columns: 45% 40% 5% 10%;
         grid-template-rows: 100%;
     }
 
-    .profileIcon{
-        height: 70px;
-        width: 70px;
-        border-radius: 50%;
-        background-color: var(--color-tailwind500);
-    }
+
+    
     
 </style>
 
@@ -47,31 +46,10 @@
     <div class="grid-item boxRed centre">
         <img src = {Logo} alt = "logo">
     </div>
-    <div class="grid-item boxRed centre topBarGrid">
-        <div class="boxBlue"> 
-            
-        </div>
-        <div class="boxBlue"></div>
-        <div class="boxBlue centre">
-            <img src = "{Bell}" alt = "Notification Bell">
-        </div>
-        <div class="boxBlue centre">
-            <span class = "profileIcon"></span>
-        </div>
-
-    </div>
-    <div class="grid-item boxRed">
-        <Menu />
-    </div>
+    <div class="grid-item boxRed"> <TopBar /> </div>
+    <div class="grid-item boxRed"> <Menu /> </div>
     <div class="grid-item boxRed">
         <p> main section</p>
     </div>
     
   </div>
-<h1>
-    Welcome to the start of an epic journey!
-</h1>
-
-<p>
-    I am designing a plant database and this is the future home page
-</p>
---
+++ src/routes/plantHome/topBar/+page.svelte
@@ -0,0 +1,39 @@
+
+<script>
+    import Bell from '$lib/images/Bell.png';
+</script>
+<style>
+    .centre{
+        display: flex;
+        justify-content: center;
+        align-items: center;
+    }
+    .boxBlue{
+        border-width: 2px;
+        border-style: solid;
+        border-color: blue;
+    }
+    .topBarGrid{
+        display: grid;
+        grid-template-columns: 45% 40% 5% 10%;
+        grid-template-rows: 100%;
+    }
+    .profileIcon{
+        height: 60px;
+        width: 60px;
+        border-radius: 50%;
+        background-color: var(--color-tailwind500);
+    }
+    
+</style>
+<div class="topBarGrid ">
+    <div class="boxBlue"> </div>
+    <div class="boxBlue"> </div>
+    <div class="boxBlue centre">
+        <img src = "{Bell}" alt = "Notification Bell">
+    </div>
+    <div class="boxBlue centre">
+        <span class = "profileIcon"></span>
+    </div>
+
+</div>
