ae8ce80e46c104d7aa5aba2b3eb120ecd6eb0a65
BuildWithCloud <127132530+BuildWithCloud@users.noreply.github.com>
2023-07-04 13:42:09 +0000

    added the temporary logo

--- src/routes/plantHome/+page.svelte
+++ src/routes/plantHome/+page.svelte
@@ -1,3 +1,68 @@
+<script>
+    import Logo from '$lib/images/Logo.png';
+</script>
+<style>
+    .grid-container {
+        display: grid;
+        grid-template-columns: 14% 86% ; /* Sets the width of two columns */
+        grid-template-rows: 9.7VH 90.3VH; /* Sets the height of two rows */
+  
+}
+    .menu {
+        display:grid;
+        grid-template-columns:100%;
+        grid-template-rows: 10% 10% 10% 10% 10%;
+    }
+
+    .boxRed {
+        border-width: 2px;
+        border-style: solid;
+        border-color: red;
+    }
+    .boxBlue{
+        border-width: 2px;
+        border-style: solid;
+        border-color: blue;
+    }
+
+    .centre{
+        display: flex;
+        justify-content: center;
+        align-items: center;
+    }
+    
+
+</style>
+
+<div class="grid-container">
+    <div class="grid-item boxRed centre">
+        <img src = {Logo} alt = "logo">
+    </div>
+    <div class="grid-item boxRed">
+        <p> Search etc</p>
+    </div>
+    <div class="grid-item boxRed menu">
+        <div class="boxBlue centre">
+            <button type="button">My Tasks</button>
+        </div>
+        <div class="boxBlue centre">
+            <button type="button">My Plants</button>
+        </div>
+        <div class="boxBlue centre">
+            <button type="button">All Plants</button>
+        </div>
+        <div class="boxBlue centre">
+            <button type="button">Wish List</button>
+        </div>
+        <div class="boxBlue centre">
+            <button type="button">Available to Harvest</button>
+        </div>
+    </div>
+    <div class="grid-item boxRed">
+        <p> main section</p>
+    </div>
+    
+  </div>
 <h1>
     Welcome to the start of an epic journey!
 </h1>
